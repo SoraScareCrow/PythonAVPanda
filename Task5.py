82a46ab5e61f5d1405101178ecd355f2b01997d5
Andrei Vegero <87813750+SoraScareCrow@users.noreply.github.com>
2023-06-16 23:58:52 +0200

    g

--- Task5.py
+++ Task5.py
@@ -1,17 +1,17 @@
-import pandas as pd
+import pandas as panda
 
-df = pd.read_csv("people_data.csv", delimiter=',')
-df = df.replace({'[^a-zA-Z0-9\s.]': ''}, regex=True)
-df = df.applymap(lambda x: x.strip() if isinstance(x, str) else x)
-df = df.fillna(df.mode().iloc[0])
-df['income'] = pd.to_numeric(df['income'], errors='coerce')
+data = panda.read_csv("people_data.csv", delimiter=',')
+data = data.replace({'[^a-zA-Z0-9\s.]': ''}, regex=True)
+data = data.applymap(lambda x: x.strip() if isinstance(x, str) else x)
+data = data.fillna(data.mode().iloc[0])
+data['income'] = panda.to_numeric(data['income'], errors='coerce')
 num_ranges = 3
-wealth_ranges = pd.qcut(df['income'], q=num_ranges, labels=False)
-min_income = df['income'].min()
-max_income = df['income'].max()
-wealth_ranges = [min_income] + list(df['income'].quantile([i / num_ranges for i in range(1, num_ranges)])) + [
+wealth_ranges = panda.qcut(data['income'], q=num_ranges, labels=False)
+min_income = data['income'].min()
+max_income = data['income'].max()
+wealth_ranges = [min_income] + list(data['income'].quantile([i / num_ranges for i in range(1, num_ranges)])) + [
     max_income]
 wealth_labels = ["Low", "Mid", "High"]
-df['wealth_category'] = pd.cut(df['income'], bins=wealth_ranges, labels=wealth_labels)
-pd.set_option('display.max_rows', None)
-print(df)
+data['wealth_category'] = panda.cut(data['income'], bins=wealth_ranges, labels=wealth_labels)
+panda.set_option('display.max_rows', None)
+print(data)
--- Task6.py
+++ Task6.py
@@ -1,13 +1,13 @@
-import pandas as pd
+import pandas as panda
 
-df = pd.read_csv("people_data.csv", delimiter=',')
-df = df.replace({'[^a-zA-Z0-9\s.]': ''}, regex=True)
-df = df.applymap(lambda x: x.strip() if isinstance(x, str) else x)
-df = df.fillna(df.mode().iloc[0])
-df['Age'] = pd.to_numeric(df['Age'], errors='coerce')
+data = panda.read_csv("people_data.csv", delimiter=',')
+data = data.replace({'[^a-zA-Z0-9\s.]': ''}, regex=True)
+data = data.applymap(lambda x: x.strip() if isinstance(x, str) else x)
+data = data.fillna(data.mode().iloc[0])
+data['Age'] = panda.to_numeric(data['Age'], errors='coerce')
 age_ranges = [0, 17, 29, 49, float('inf')]
 age_labels = ['0-17', '18-29', '30-49', '50+']
-df['age_group'] = pd.cut(df['Age'], bins=age_ranges, labels=age_labels)
-age_group_count = df.groupby(['Country', 'age_group']).size().unstack(fill_value=0)
-pd.set_option('display.max_rows', None)
+data['age_group'] = panda.cut(data['Age'], bins=age_ranges, labels=age_labels)
+age_group_count = data.groupby(['Country', 'age_group']).size().unstack(fill_value=0)
+panda.set_option('display.max_rows', None)
 print(age_group_count)
